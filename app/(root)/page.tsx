a2494c6a2e52f8f8db0a9aae8bbb748fa2d0820b
Ronan Kitten <ronank.dev@gmail.com>
2024-06-21 10:19:48 +0700

    Initial Commit - add sentry, add appwrite, add sign in & sign up functionality and UI, add bank card UI, add dashboard UI, add sidebar, add right sidebar

---
+++ app/(root)/page.tsx
@@ -0,0 +1,38 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import React from "react";
+
+const Home = async () => {
+	const loggedIn = await getLoggedInUser();
+
+	return (
+		<section className="home">
+			<div className="home-content">
+				<header className="home-header">
+					<HeaderBox
+						type="greeting"
+						title="Welcome"
+						user={loggedIn?.name || "Guest"}
+						subtext="Access and manage your account and transactions effeciently"
+					/>
+
+					<TotalBalanceBox
+						accounts={[]}
+						totalBanks={1}
+						totalCurrentBalance={1250.35}
+					/>
+				</header>
+				Recent Transactions
+			</div>
+			<RightSidebar
+				user={loggedIn}
+				transactions={[]}
+				banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+			/>
+		</section>
+	);
+};
+
+export default Home;
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://20a01ba4a206857c58f8fec080f255e2@o4507467234476032.ingest.us.sentry.io/4507467245486080",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
